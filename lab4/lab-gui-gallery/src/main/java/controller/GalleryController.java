b70f558f19bfcce35ea154c2f06db6187657746e
AlexMaz99 <ola_m10@onet.pl>
2020-11-04 17:50:31 +0100

    Started lab4

---
+++ lab4/lab-gui-gallery/src/main/java/controller/GalleryController.java
@@ -0,0 +1,40 @@
+package controller;
+
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import model.Gallery;
+import model.Photo;
+
+
+
+public class GalleryController {
+
+    private Gallery galleryModel;
+
+    @FXML
+    private TextField imageNameField;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        // TODO additional FX controls initialization
+    }
+
+    public void setModel(Gallery gallery) {
+        this.galleryModel = gallery;
+        bindSelectedPhoto(gallery.getPhotos().get(0));
+    }
+
+    private void bindSelectedPhoto(Photo selectedPhoto) {
+        imageView.imageProperty().bind(selectedPhoto.imageProperty());
+        imageNameField.textProperty().bind(selectedPhoto.nameProperty());
+
+    }
+
+}
+
